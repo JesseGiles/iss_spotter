9bac366c4b2e4a4f168bfaf81961d2537f408efb
Jesse Giles <jesse.giles@telus.com>
2022-11-24 18:59:26 -0700

    finished programming all 3 functions of iss.js and verifying results populating through index.js

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 // index.js
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
 
-//   console.log('It worked! Returned IP:' , ip);
-// });
+  console.log('It worked! Returned IP:' , ip);
+});
 
-// fetchCoordsByIP('104.205.131.163', (error, data) => {
+fetchCoordsByIP('104.205.131.163', (error, data) => {
   
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
 
-//   console.log('It worked! Heres some data:', data);
-// });
+  console.log('It worked! Heres some data:', data);
+});
+
+fetchISSFlyOverTimes({ latitude: '55.1699396', longitude: '-118.7986152' }, (error, data) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Heres the flight data:', data);
+
+});
